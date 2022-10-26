c8a5af3ed852211d0b895cf7347054787a0e9582
MaryyamSalah <maryyamslah20@gmail.com>
2022-10-26 15:13:38 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solutions in this file
+
+
+
+//assignment
+const arr = ['maryam','salah','sh']
+function writeCards(arr){
+    let newArr=[]
+for (let i=0 ; i<arr.length ; i++){
+    newArr.push(`Thank you ,${arr[i]} for the wonderful surprise gift!`)
+    debugger
+}
+return newArr
+}
+writeCards(arr)
+
+
+
+//count down while
+
+function countDown (){
+    let count = 10
+
+while (count > -1) {
+  console.log(count--);
+}}
